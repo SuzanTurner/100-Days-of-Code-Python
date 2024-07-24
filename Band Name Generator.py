be748d28ac1e5ecc1a152754f53c83e0e8af2397
Yadhnika Wakde <yadhnikawakde@gmail.com>
2024-07-24 15:09:35 +0530

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+print("Welcome to bang name generator")
+city = input("Enter the city you grew up in: ")
+pet = input("Enter your pet name: ")
+print("Your band name is " + city + " " + pet)
